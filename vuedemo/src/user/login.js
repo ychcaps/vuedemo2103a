055c8fc9253a48c5dc40dc37f7a1cbf29d7bd388
ychcaps <415354750@qq.com>
2021-12-20 20:16:09 +0800

    qqqqq

---
+++ vuedemo/src/user/login.js
@@ -0,0 +1,10 @@
+//登录界面 组件
+const loginForm = {
+    template:'\
+    <div>\
+    <h2>登录页</h2> \
+    用户名：<input type="text"><br/>\
+    密&emsp;码：<input type="password"><br/>\
+    </div>\
+    '
+}
